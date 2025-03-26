a0ff71be890d58a8098f9fd19db2fc73889b15f6
HuyLle <lehuy23701@gmail.com>
2025-03-26 09:58:04 +0700

    .

--- Assets/Script/ObjectiveReachPoint.cs
+++ Assets/Script/ObjectiveReachPoint.cs
@@ -5,9 +5,12 @@ public class ObjectiveReachPoint : MonoBehaviour
     public Animator anim; // Animator của NextFloor
     public bool isFirstStage = false;
     //public SceneLoader loader;
+    private AudioSource audioSource;
+    public AudioClip winSound;
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         if (anim == null && isFirstStage)
             anim = GetComponentInParent<Animator>(); // Tìm Animator trên NextFloor
     }
@@ -25,6 +28,7 @@ public class ObjectiveReachPoint : MonoBehaviour
             }
             else 
             {
+                audioSource.PlayOneShot(winSound);
                 if(SceneLoader.Instance != null)
                 {
                     SceneLoader.Instance.LoadSceneByIndex(3);
